2e8f66961c38f9364b672bbc4ecdb374931d3b6d
irfan_akgul <mrakgul.nl@outlook.com>
2026-03-21 19:22:06 +0100

    mail service updated

---
+++ main_mail_test.py
@@ -0,0 +1,13 @@
+from app.services.email_service import send_email
+
+send_email(
+    to_email=["1irfanakgul@gmail.com","yasinyilmaz19@gmail.com","Pidatasmanba@gmail.com"],
+    subject="Trade Alert Test",
+    title="New Trade Signal",
+    body="""
+Symbol: TEST
+Signal: BUY
+Strategy: Opening Range
+""".strip(),
+    action_link="https://docs.google.com/spreadsheets/d/1jdJkgQo1Y5tLFOUjI8Y_Ole8P4VArDtP0H-n5FJ-Zg4/edit?gid=1067836751#gid=1067836751"
+)
